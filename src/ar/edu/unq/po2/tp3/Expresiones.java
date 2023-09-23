85295d1e3d7674a66ef6c0f005b6352a3284ec61
NavaG712 <you@example.com>
2023-09-23 20:17:48 -0300

    Tipos primitivos (Dentro de Expresiones como comentario)

--- src/ar/edu/unq/po2/tp3/Expresiones.java
+++ src/ar/edu/unq/po2/tp3/Expresiones.java
@@ -17,4 +17,12 @@ public class Expresiones {
 		boolean b = s == a; //Devuelve False
 		a.substring(1,3).equals("bc"); //Devuelve True
 	}
+	
+	/* TIPOS PRIMITIVOS
+	 * 1. ¿Qué son los tipos primitivos?. Son los datos de un lenguaje de programación que vienen por defecto, o sea, los no referenciados
+	 * 2. ¿Cuál es la diferencia entre un int y un Integer?. int es un tipo primitivo de Java, Integer se lo conoce como un wrapper de int, en
+	 * si es un tipo objeto
+	 * 3.¿Si se define una variable de instancia de tipo int cual sería su valor predeterminado? ¿Y si se define una de tipo Integer?
+	 * En el int es 0, en el Integer es null
+	 * 4.Ahora en lugar de definir una variable de instancia se define una variable de método. Es el mismo resultado*/
 }
