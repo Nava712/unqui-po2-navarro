327429eeece540f789d4b439b64b5a2c58700af6
NavaG712 <you@example.com>
2023-09-23 21:47:14 -0300

    arreglo en expresiones

--- src/ar/edu/unq/po2/tp3/Expresiones.java
+++ src/ar/edu/unq/po2/tp3/Expresiones.java
@@ -8,13 +8,13 @@ public class Expresiones {
 	public void test() {
 		s.length(); //Devuelve 1
 		t.length(); //Devuelve NullPointerException, debido a que t es un string no inicializado, por lo que es NULL
-		String p = 1 + a; //Devuelve "1abc"
+		//String p = 1 + a; //Devuelve "1abc"
 		a.toUpperCase(); //Devuelve ABC
 		"Libertad".indexOf("r"); //Devuelve 4
 		"Universidad".lastIndexOf('i'); //Devuelve 7
 		"Quilmes".substring(2,4); //Devuelve "il"
 		(a.length() + a).startsWith("a"); //Devuelve False
-		boolean b = s == a; //Devuelve False
+		//boolean b = s == a; //Devuelve False
 		a.substring(1,3).equals("bc"); //Devuelve True
 	}
 	
